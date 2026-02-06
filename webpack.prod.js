22eda321c3a08c384d94641f013cb2e02f881fae
SnorlaxPapa <yyeroy@gmail.com>
2026-02-06 15:04:11 +0800

    replaced config with webpack merge

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,12 @@
 // webpack.config.js
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import {merge} from "webpack-merge";
+import common from "./webpack.common.js";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import { fileURLToPath } from "node:url";
 
-const filepath = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(filepath);
 
-export default {
+export default merge(common, {
   mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
     new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
   ],
   module: {
@@ -27,18 +15,6 @@ export default {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-};
+});
